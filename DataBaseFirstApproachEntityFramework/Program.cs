169083e3ce082877611d4361127536b7537887e2
Nilesh Shivale <n.shivale@tricentis.com>
2018-03-21 14:24:22 +0530

    FirstCommit

---
+++ DataBaseFirstApproachEntityFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirstApproachEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PracticeSelfEntities();
+            var post = new PostsTable()
+            { Body="Body",
+            DatePublished=DateTime.Now,
+            Title="Title",
+            PostID=1
+            };
+
+            context.PostsTables.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
